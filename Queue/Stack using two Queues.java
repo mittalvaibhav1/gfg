3d1dca63f0b561888ba20a297fad89d6d5019112
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-26 23:58:37 +0530

    Stack using two Queues M2

--- Queue/Stack using two Queues.java
+++ Queue/Stack using two Queues.java
@@ -35,3 +35,39 @@ class Queues
     }
 }
 
+// M2
+
+// O(n) - pop and O(1) - push
+
+class Queues
+{
+    Queue<Integer> q1 = new LinkedList<Integer>();
+    Queue<Integer> q2 = new LinkedList<Integer>();
+    
+
+    int pop()
+    {
+        if(q1.isEmpty()) {
+            return -1;
+        }
+        else {
+            while(q1.size() != 1) {
+                q2.add(q1.poll());
+            }
+            int popped = q1.poll();
+            while(!q2.isEmpty()) {
+                q1.add(q2.poll());
+            }
+            return popped;
+        }
+    }
+    
+
+    void push(int a)
+    {
+        q1.add(a);
+    }
+}
+
+
+
